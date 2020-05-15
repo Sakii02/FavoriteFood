6514522cadebd0f78ccb1866f1848e034d5de2c5
nanagin <nanagin@192.168.10.3>
2020-05-15 19:34:42 +0900

    変更しました。

--- favoritefood/src/favoritefood/Main.java
+++ favoritefood/src/favoritefood/Main.java
@@ -1,11 +1,8 @@
 package favoritefood;
-import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-
-		Scanner sc = new Scanner(System.in);
 
 		System.out.println("好きな食べ物はさくらんぼです。");
 		System.out.println("甘くて美味しいからです。");
