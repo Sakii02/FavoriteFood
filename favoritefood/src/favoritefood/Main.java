823e4bc93d323458058b7df9ae3d77e566bcfebb
ayaka <ayaka@localhost>
2020-05-17 15:38:44 +0900

    返答を更新しました。

--- favoritefood/src/favoritefood/Main.java
+++ favoritefood/src/favoritefood/Main.java
@@ -6,8 +6,9 @@ public class Main {
 
 		System.out.println("好きな食べ物はさくらんぼです。");
 		System.out.println("甘くて美味しいからです。");
-		
+
 		System.out.println("さくらんぼは美味しくて色も綺麗ですね by岡");
+		System.out.println("さくらんぼは形も可愛くて、ついついさくらんぼグッズも欲しくなっちゃうよね！ by あや");
 	}
 
 }
